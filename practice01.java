f8348e52201ee780d8321c748471dc3d086fe657
Andy 曹佳成 <48340515+lalizhi@users.noreply.github.com>
2019-05-17 11:12:24 +0800

    Create practice01.java

---
+++ practice01.java
@@ -0,0 +1,13 @@
+public class fire{
+    public static void main(String[] args) {
+        int age = 18;
+        int user = 6;
+        boolean is = false;
+        if (user>= age)
+            System.out.println("1You can vote");
+            if (is)
+                System.out.println("2I like java");
+                else
+                System.out.println(3.);
+    }
+}
